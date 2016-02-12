7d90b485185388d5acb502b3ce43ae1b4bb2b281
Tim Collinson <trcollinson@gmail.com>
2016-02-12 10:27:27 -0700

    Updated with information from part 4.

---
+++ app/hero.service.ts
@@ -0,0 +1,10 @@
+import {HEROES} from './mock-heroes';
+import {Injectable} from 'angular2/core';
+
+@Injectable()
+
+export class HeroService {
+	getHeroes() {
+		return Promise.resolve(HEROES);
+	}
+}
